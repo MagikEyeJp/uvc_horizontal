7b07d6b115c2f4ab8afdb11410a33ea899f4f011
uribo <ur1b0.dev@gmail.com>
2022-02-01 23:03:36 +0900

    ボーダーラインの表示切り替えを追加

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2 as cv
 import numpy as np
 
+FLG_MASK = False
+
 def show_data_array(data):
     print("type: ", type(data))
     print("ndim: ", data.ndim)
@@ -11,7 +13,6 @@ def show_data_array(data):
 vid = cv.VideoCapture(0)
 ret, frame = vid.read()
 
-# image size or you can get this from image shape
 frame_width = vid.get(cv.CAP_PROP_FRAME_WIDTH)
 frame_height = vid.get(cv.CAP_PROP_FRAME_HEIGHT)
 video_fps = vid.get(cv.CAP_PROP_FPS)
@@ -21,18 +22,20 @@ print(video_fps)
 
 show_data_array(frame)
 
-mask = np.full_like(frame,255)
-mask = cv.rectangle(mask, (319,0), (321,480), (0,0,255), thickness=-1)
-mask = cv.rectangle(mask, (0,239), (640,241), (0,0,255), thickness=-1)
+mask = np.full_like(frame,0)
+mask = cv.rectangle(mask, (319,0), (321,480), (0,0,170), thickness=-1)
+mask = cv.rectangle(mask, (0,239), (640,241), (0,0,170), thickness=-1)
 
 while True:
     ret, frame = vid.read()
-#    cv.imshow('frame', frame)
-    cv.imshow('frame', cv.bitwise_and(frame,mask))
-#    cv.imshow('frame', cv.addWeighted(src1=frame,alpha=0.8,src2=mask,beta=0.2,gamma=0))
-#    cv.imshow('frame', frame+mask)
-    if cv.waitKey(1) & 0xFF == ord('q'):
+    cv.imshow('frame', (frame^mask) if FLG_MASK else frame )
+    print(FLG_MASK)
+
+    key = cv.waitKey(1)
+    if key == ord('q'):
         break
+    elif key == ord('m'):
+        FLG_MASK = not FLG_MASK
 
 vid.release()
 cv.destroyAllWindows()
