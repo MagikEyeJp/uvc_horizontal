0353a4a132bbe49004d285ebc92c49486bd6c26f
uribo <ur1b0.dev@gmail.com>
2022-02-02 21:53:04 +0900

    リファクタした

--- main.py
+++ main.py
@@ -16,31 +16,21 @@ CMD_STRB_OFF = "v4l2-ctl -d 0 --set-ctrl=strobe_enable=0"
 def set_strobe(state):
     cmd = CMD_STRB_ON if state else CMD_STRB_OFF
     ret = ss.Popen(cmd, stdout=ss.PIPE, shell=True).communicate()
-    print(cmd)
+    print("strobe:"+str(state))
 
-def show_data_array(data):
-    print("type: ", type(data))
-    print("ndim: ", data.ndim)
-    print("shape: ", data.shape)
-    print("size: ", data.size)
-    print("dtype: ", data.dtype)
-
+def show_video_info(v):
+    print("width:"+str(vid.get(cv.CAP_PROP_FRAME_WIDTH)))
+    print("height:"+str(vid.get(cv.CAP_PROP_FRAME_HEIGHT)))
+    print("fps:"+str(vid.get(cv.CAP_PROP_FPS)))
+    print("buffer:"+str(vid.get(cv.CAP_PROP_BUFFERSIZE)))
+    
 vid = cv.VideoCapture(0)
 vid.set(cv.CAP_PROP_FRAME_WIDTH, WIDTH)
 vid.set(cv.CAP_PROP_FRAME_HEIGHT, HEIGHT)
 vid.set(cv.CAP_PROP_BUFFERSIZE, 1)
+show_video_info(vid)
+
 ret, frame = vid.read()
-
-frame_width = vid.get(cv.CAP_PROP_FRAME_WIDTH)
-frame_height = vid.get(cv.CAP_PROP_FRAME_HEIGHT)
-video_fps = vid.get(cv.CAP_PROP_FPS)
-buffer = vid.get(cv.CAP_PROP_BUFFERSIZE)
-print(frame_width)
-print(frame_height)
-print(video_fps)
-print(buffer)
-
-show_data_array(frame)
 
 while True:
     ret, frame = vid.read()
@@ -49,8 +39,6 @@ while True:
         frame = cv.line(frame, (int(WIDTH/2),0), (int(WIDTH/2),HEIGHT), COLOR, thickness=2)
         frame = cv.line(frame, (0,int(HEIGHT/2)), (WIDTH,int(HEIGHT/2)), COLOR, thickness=2)
     cv.imshow('frame', frame)
-#    cv.imshow('frame', (frame^mask) if FLG_MASK else frame )
-#    print(FLG_MASK)
 
     key = cv.waitKey(1)
     if key == ord('q'):
